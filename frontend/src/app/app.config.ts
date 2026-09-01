befcba411d4cd8299ddf9fd1bf6762271646a08f
PxS <lucas.rossonidd@gmail.com>
2026-09-01 01:19:51 -0300

    chore(frontend): configure HTTP foundation (#110)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { API_BASE_URL } from './core/config/api-base-url.token';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [ provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient(),
+    {
+      provide: API_BASE_URL,
+      useValue: environment.apiBaseUrl,
+    },
+  ],
 };
